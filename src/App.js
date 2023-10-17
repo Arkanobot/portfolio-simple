fe850e17d94baf303b3a34a25fed145aff51642a
Shreyas Bhat K <67960703+Arkanobot@users.noreply.github.com>
2023-10-18 04:40:20 +0530

    removing hashrouter

--- src/App.js
+++ src/App.js
@@ -5,22 +5,20 @@ import HomePage from "./components/HomePage";
 import Employment from "./components/Employment";
 import Projects from "./components/Projects";
 import Education from "./components/Education";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <HashRouter basename="/">
-          <Header />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/Projects" element={<Projects />} />
-            <Route path="/Employment" element={<Employment />} />
-            <Route path="/Education" element={<Education />} />
-          </Routes>
-          <Footer />
-        </HashRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Employment" element={<Employment />} />
+          <Route path="/Education" element={<Education />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
